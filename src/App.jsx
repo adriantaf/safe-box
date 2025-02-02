2ff1b99535b418d05f55c94c81c1e9fd9d9f61af
adriantaf <atafoya863@gmail.com>
2025-02-02 14:16:06 -0800

    Pequeño cambio de un contenedor

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,20 @@ function App() {
         <Header />
         <main className='px-4 pb-2 pt-0 overflow-y-auto overflow-x-hidden'>
           { dataOfDB === null ? (
-            <div className="h-full flex justify-center items-center border bg-neutral-50 border-gray-300 dark:border-gray-600 dark:bg-slate-950 rounded-lg shadow-lg">
-              <h2 className='font-medium text-xl'>Cargando datos...</h2>
-            </div>
+            <section>
+              <div className="h-full flex justify-center items-center border bg-neutral-50 border-gray-300 dark:border-gray-600 dark:bg-slate-950 rounded-lg shadow-lg">
+                <h2 className='font-medium text-xl'>Cargando datos...</h2>
+              </div>
+            </section>
           ) : dataOfDB.length === 0 ? (
-            <div className='h-full flex justify-center flex-col items-center border bg-neutral-50 border-gray-300 dark:border-gray-600 dark:bg-slate-950 rounded-lg shadow-lg'>
-              <h2 className='font-medium text-xl'>
-                Aún no has guardado ninguna contraseña.
-              </h2>
-              <Key className="size-20 text-neutral-400" />
-            </div>
+            <section className='h-full w-full pt-4 pb-2'>
+              <div className='h-full flex justify-center flex-col items-center border bg-neutral-50 border-gray-300 dark:border-gray-600 dark:bg-slate-950 rounded-lg shadow-lg'>
+                <h2 className='font-medium text-xl'>
+                  Aún no has guardado ninguna contraseña.
+                </h2>
+                <Key className="size-20 text-neutral-400" />
+              </div>
+            </section>
           ) : (
             <Table dataFromDB={ dataOfDB } />
           ) }
